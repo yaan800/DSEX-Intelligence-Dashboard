c3af3a8ec44db7414b08c1d6ea96b33e506ba670
yaan800 <57150616+yaan800@users.noreply.github.com>
2026-04-07 11:11:22 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,7 @@ const __dirname = path.dirname(__filename);
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), viteSingleFile()],
+  base: '/DSEX-Intelligence-Dashboard/'
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
